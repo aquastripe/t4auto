3a6450dd0099f856dd176c48e84f9be09ac4f3b6
Hao-Ting Li <maniac@csie.io>
2024-06-15 18:57:09 +0800

    chore: remove unused function call
    Took 3 minutes

--- t4auto.py
+++ t4auto.py
@@ -9,7 +9,6 @@ def main():
     app = QApplication(sys.argv)
     app.setApplicationName('T4 Auto')
     t4auto_gui = T4AutoGUI()
-    t4auto_gui.resize(800, 600)
     t4auto_gui.show()
     app.exec()
 
