a23f9a4d75ef0d24be6604a482c85fa2121a94fd
Hao-Ting Li <maniac@csie.io>
2024-06-16 02:39:00 +0800

    fix: correct restoring WindowSize from config
    Took 16 minutes

--- t4autolibs/gui/window_size.py
+++ t4autolibs/gui/window_size.py
@@ -6,58 +6,67 @@ from PySide6.QtWidgets import QWidget, QVBoxLayout, QRadioButton, QGroupBox
 
 from t4autolibs.gui.config import Configurable
 
-FLAG_MAXIMIZE = 0
-
 
 @dataclass
 class SizeInfo:
+    idx: int
     display_name: str
-    size: [tuple[int, int], int]
+    size: tuple[int, int]
+
+
+IDX_MAXIMIZE_WINDOW = 3
 
 
 class WindowSize(Configurable):
     _SIZE_INFO_LIST = [
-        SizeInfo('800 × 600', (800, 600)),
-        SizeInfo('1024 × 768', (1024, 768)),
-        SizeInfo('1920 × 1080', (1920, 1080)),
-        SizeInfo('Maximize', FLAG_MAXIMIZE),
+        SizeInfo(0, '800 × 600', (800, 600)),
+        SizeInfo(1, '1024 × 768', (1024, 768)),
+        SizeInfo(2, '1920 × 1080', (1920, 1080)),
+        SizeInfo(IDX_MAXIMIZE_WINDOW, 'Maximize', (0, 0)),
     ]
 
     def __init__(self, parent_window: QWidget):
-        self.size = self._SIZE_INFO_LIST[0].size
+        self.size_info = self._SIZE_INFO_LIST[0]
         self.parent_window = parent_window
-        self.resize_main_window(self.size)
+        self.resize_main_window(self._SIZE_INFO_LIST[0])
         self.window_size_layout = QVBoxLayout()
 
         first_button_is_checked = False
+        self.buttons = []
         for size_info in self._SIZE_INFO_LIST:
             button = QRadioButton(size_info.display_name)
             if not first_button_is_checked:
                 button.setChecked(True)
                 first_button_is_checked = True
 
-            specified_resize_main_window = partial(self.resize_main_window, size=size_info.size)
+            specified_resize_main_window = partial(self.resize_main_window, size_info=size_info)
             button.clicked.connect(specified_resize_main_window)
             self.window_size_layout.addWidget(button)
+            self.buttons.append(button)
 
         self.group = QGroupBox('Window Size')
         self.group.setLayout(self.window_size_layout)
 
-    def resize_main_window(self, size):
-        self.size = size
-        if self.size == FLAG_MAXIMIZE:
+    def resize_main_window(self, size_info: SizeInfo):
+        self.size_info = size_info
+        if self.size_info.idx == IDX_MAXIMIZE_WINDOW:
             self.parent_window.showMaximized()
         else:
-            self.parent_window.resize(*self.size)
+            self.parent_window.resize(*self.size_info.size)
 
     def load_config(self, config: dict) -> NoReturn:
         if config['WindowSize'] is None:
             return
 
-        self.resize_main_window(config['WindowSize'])
+        if config['WindowSize'] >= len(self._SIZE_INFO_LIST):
+            print(f'config.json Warning: The WindowSize is too large. It should be < {len(self._SIZE_INFO_LIST)}.')
+            return
+
+        size_info = self._SIZE_INFO_LIST[config['WindowSize']]
+        self.buttons[size_info.idx].click()
 
     def dump_config(self) -> dict:
         config = {
-            'WindowSize': self.size,
+            'WindowSize': self.size_info.idx,
         }
         return config
